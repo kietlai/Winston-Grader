9b519374b771031a0ec5b16b1898f655d779267c
Kiet Lai <kietlai411@gmail.com>
2022-05-21 15:07:52 -0400

    Second Commit
    Lays out basic framework for the project.

---
+++ app.py
@@ -0,0 +1,19 @@
+from pydoc import render_doc
+from flask import Flask, render_template, request, flash, redirect, url_for
+
+
+app = Flask('app')
+
+@app.route('/')
+def home():
+  return render_template('home.html')
+
+@app.route('/gpa')
+def gpa():
+  return render_template('gpa.html')
+
+#todo: Add more routes and use redirects to change some things. 
+#todo: add in a home page, gpa, grade affect, save it to a server/database. 
+#todo: use jinja to create templates. 
+#todo: finish up the README markdown file
+#todo: far future create a sign up page + login page. 
