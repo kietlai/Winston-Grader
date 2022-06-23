68e41add6d5b8c9c8ea1f9731e651743f139f597
Nathan Story <56227357+NRS048@users.noreply.github.com>
2022-06-23 17:57:39 -0400

    Update Grader.py

--- Grader.py
+++ Grader.py
@@ -1,15 +1,33 @@
-import math
-
-#assigment vars will be named with "a" followed by a number
-#may use a list or a dictainary which could speed up the work flow and ease of use. 
-#will use sql later on to fix all of these issues without using a dictaniry and creating a new one everytime. 
-#Possible List use. 
-
-
 grades = [98,38,69]
+max_grades = [100,50,75]
 
+earned_perc = []
 
-mGrades = [100,50,75]
+final_percent = 0
+grey_percent = 0
 
-for i in grades:
-  print(i)
+def calc_perc (grades, max_grades):
+    i = 0
+    while i < len(grades):
+        earned_perc.append(grades[i]/max_grades[i])
+        i += 1
+
+    return earned_perc
+
+def calc_average(num):
+    sum_num = 0
+    for t in num:
+        sum_num = sum_num + t           
+
+    avg = sum_num / len(num)
+    return avg
+
+final_percent = round((calc_average(calc_perc(grades, max_grades))), 2)
+final_percent *= 100
+def calc_grey (final_percent):
+    return 100 - final_percent
+
+grey_percent = round(calc_grey(final_percent), 2)
+
+print("final percent " + str(final_percent))
+print("grey percent " + str(grey_percent))
