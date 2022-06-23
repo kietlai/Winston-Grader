e5cabb3f6ea450040d3dd46ffb0ab421cb23bae8
Kiet Lai <kietlai411@gmail.com>
2022-06-23 11:14:59 -0400

    Working on grader

--- Grader.py
+++ Grader.py
@@ -3,8 +3,17 @@ import math
 #assigment vars will be named with "a" followed by a number
 #may use a list or a dictainary which could speed up the work flow and ease of use. 
 #will use sql later on to fix all of these issues without using a dictaniry and creating a new one everytime. 
+#Possible List use. 
 
 
 grades = {
-
+  #grades
+  "g1": 98,
+  "g2": 38
 }
+mGrades = {
+  #P followed by a number is the Points that is used.
+  "p1": 100,
+  "p2": 50
+}
+
--- app.py
+++ app.py
@@ -1,3 +1,6 @@
+#Name: Kiet Lai and Nathan story
+
+
 from pydoc import render_doc
 from flask import Flask, render_template, request, flash, redirect, url_for
 
@@ -12,11 +15,17 @@ def home():
 def gpa():
   return render_template('gpa.html')
 
+@app.route("/grader")
+def grader():
+  return render_template('grader.html')
+
+
+
 #todo: Add more routes and use redirects to change some things. 
 #todo: add in a home page, gpa, grade affect, save it to a server/database. 
 #todo: use jinja to create templates. 
 #todo: finish up the README markdown file
 #todo: far future create a sign up page + login page.
 #todo: later create a system to take notes that will be on a contender with others. 
-#Open source???
 #todo: create a user saved page. 
+#Open source???
