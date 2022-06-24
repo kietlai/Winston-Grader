e3dad51876edfc52ab21798e76f00a3c9a2135db
Kiet Lai <kietlai411@gmail.com>
2022-06-23 22:09:06 -0400

    ReadMe and some minor

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
-Winston Grader
+# Winston Grader
+### By: Kiet Lai and Nathan Story
+
+## Some basic info
+This is a project that is a **suite for students**.
+
+The things that this project will have the following tools within.
+1. GPA calculator
+2. Grader (This will show you the percantage that each assigement will have an affect on.)
+3. Overall grade
+4. Radar scale (To show which subjects that excel in)
+   
+How this will be present. It will be present in a form using bar graphs, pie charts or donut charts. Some additions may be polar area chart or radar charts, to show you strengths in the overall scheme of things. 
+
+## Technology 
+The tech used. This is not final
+
+- Flask 
+- ChartJs
+- SQLAlchemy
+- SQLite
+- Bootstrap
--- app.py
+++ app.py
@@ -1,5 +1,13 @@
 #Name: Kiet Lai and Nathan story
 
+#todo: Add more routes and use redirects to change some things. 
+#todo: add in a home page, gpa, grade affect, save it to a server/database. 
+#todo: use jinja to create templates. 
+#todo: finish up the README markdown file
+#todo: far future create a sign up page + login page.
+#todo: later create a system to take notes that will be on a contender with others. 
+#todo: create a user saved page. 
+#Open source???
 
 from pydoc import render_doc
 from flask import Flask, render_template, request, flash, redirect, url_for
@@ -21,11 +29,3 @@ def grader():
 
 
 
-#todo: Add more routes and use redirects to change some things. 
-#todo: add in a home page, gpa, grade affect, save it to a server/database. 
-#todo: use jinja to create templates. 
-#todo: finish up the README markdown file
-#todo: far future create a sign up page + login page.
-#todo: later create a system to take notes that will be on a contender with others. 
-#todo: create a user saved page. 
-#Open source???
