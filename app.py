6de3f858c06b65fd1bc3d9c45fcf626bcc9e8bd4
Kiet Lai <kietlai411@gmail.com>
2022-06-28 12:21:05 -0400

    Adding in content enter page

--- app.py
+++ app.py
@@ -1,4 +1,7 @@
-#Name: Kiet Lai and Nathan story
+import json
+from flask import Flask, render_template, request, flash, redirect, url_for
+
+#Name: Kiet Lai, Nathan Story, and Jason (I DONT KNOW HOW TO SPELL YOUR LAST NAME.)
 
 #todo: Add more routes and use redirects to change some things. 
 #todo: add in a home page, gpa, grade affect, save it to a server/database. 
@@ -7,10 +10,10 @@
 #todo: far future create a sign up page + login page.
 #todo: later create a system to take notes that will be on a contender with others. 
 #todo: create a user saved page. 
+#REMOVE post and get from all other methods
 #Open source???
 
-from pydoc import render_doc
-from flask import Flask, render_template, request, flash, redirect, url_for
+
 
 
 app = Flask('app')
@@ -19,13 +22,14 @@ app = Flask('app')
 def home():
   return render_template('content/home.html')
 
-@app.route('/gpa')
+@app.route('/gpa', methods = ['POST', 'GET'])
 def gpa():
   return render_template('dashboard/gpa.html')
 
-@app.route("/grader")
+@app.route('/grader', methods = ['POST', 'GET'])
 def grader():
   return render_template('dashboard/grader.html')
 
-
-
+@app.route('/grade-enter/', methods = ['POST', 'GET'])
+def enter():
+  return render_template('/content/enter.html')
